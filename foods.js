e04cd1daeda5b3a581d812845f0fc539327cc06d
Max Glenister <moglenstar@gmail.com>
2022-03-04 18:08:58 +0000

    Moved foods out to separate file and changed the UI a bit

---
+++ foods.js
@@ -0,0 +1,42 @@
+const foodsData = [
+  {
+    "name": "Potato (uncooked)",
+    "carbs": 17.0
+  },
+  {
+    "name": "Pasta (uncooked)",
+    "carbs": 75.0
+  },
+  {
+    "name": "Whole wheat lasagne (uncooked)",
+    "carbs": 66.0
+  },
+  {
+    "name": "Rice (white, uncooked)",
+    "carbs": 80.0
+  },
+  {
+    "name": "Sweet Potato (uncooked)",
+    "carbs": 20.0
+  },
+  {
+    "name": "Parsnips (uncooked)",
+    "carbs": 18.0
+  },
+  {
+    "name": "Butternut Squash (uncooked)",
+    "carbs": 12.0
+  },
+  {
+    "name": "Carrot (uncooked)",
+    "carbs": 10.0
+  },
+  {
+    "name": "Couscous (uncooked)",
+    "carbs": 77.43
+  },
+  {
+    "name": "Flour",
+    "carbs": 76.0
+  }
+];
